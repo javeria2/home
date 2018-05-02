525329a4a8ccf39844eff1500017ab0c33987d00
Sanchay Javeria <javeria2@illinois.edu>
2018-05-02 03:52:09 -0500

    add router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom'
 import { Parallax } from 'react-spring'
 import resume from './resume.pdf'
 import './styles.css'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 
 const Page = ({ offset, caption, first, second, gradient, onClick }) => (
   <React.Fragment>
@@ -34,29 +38,41 @@ const Page = ({ offset, caption, first, second, gradient, onClick }) => (
   </React.Fragment>
 )
 
-
-class App extends React.Component {
-  scroll = to => this.refs.parallax.scrollTo(to)
+class Home extends React.Component {
   render() {
     return (
       <div>
         <Parallax className="container" ref="parallax" pages={4} horizontal scrolling={false}>
-          <Page offset={0} gradient="pink" caption="I am 👋" first="Sanchay Javeria" second="" onClick={() => this.scroll(1)} />
-          <Page offset={1} gradient="teal" caption="what I do 💻" first="SWE Intern at Pinterest" second="past Yahoo! Inc." onClick={() => this.scroll(2)} />
-          <Page offset={2} gradient="tomato" caption="Besides programming, I like 🌊" first="Working out, Traveling, comics" second="EDM, Billiards, anime" onClick={() => this.scroll(3)} />
-          <Page offset={3} gradient="potato" caption="Get in touch ☕" first="sanchay [dot] javeria" second="[at] gmail [dot] com" onClick={() => this.scroll(0)} />
+          <Page offset={0} gradient="pink" caption="I am 👋" first="Sanchay Javeria" second=""
+            onClick={() => this.refs.parallax.scrollTo(1)} />
+          <Page offset={1} gradient="teal" caption="what I do 💻" first="SWE Intern at Pinterest" second="past Yahoo! Inc."
+            onClick={() => this.refs.parallax.scrollTo(2)} />
+          <Page offset={2} gradient="tomato" caption="Besides programming, I like 🌊" first="Working out, Traveling, comics" second="EDM, Billiards, anime"
+            onClick={() => this.refs.parallax.scrollTo(3)} />
+          <Page offset={3} gradient="potato" caption="Get in touch ☕" first="sanchay [dot] javeria" second="[at] gmail [dot] com"
+            onClick={() => this.refs.parallax.scrollTo(0)} />
         </Parallax>
         <div style={styles.footer}>
           <p>
-            <a title="Resume" href={ resume }><i class="fas fa-file-alt"></i></a>
-            <a title="Github" href="https://www.github.com/javeria2/"><i class="fab fa-github-alt"></i></a>
-            <a title="LinkedIn" href="https://www.linkedin.com/in/javeria2/"><i class="fab fa-linkedin"></i></a>
-            <a title="Medium" href="https://medium.com/@sanchay.javeria"><i class="fab fa-medium"></i></a>
-            <a title="Facebook" href="https://www.facebook.com/sanchay.javeria"><i class="fab fa-facebook"></i></a>
+            <a title="Resume" href={ resume }><i className="fas fa-file-alt"></i></a>
+            <a title="Github" href="https://www.github.com/javeria2/"><i className="fab fa-github-alt"></i></a>
+            <a title="LinkedIn" href="https://www.linkedin.com/in/javeria2/"><i className="fab fa-linkedin"></i></a>
+            <a title="Medium" href="https://medium.com/@sanchay.javeria"><i className="fab fa-medium"></i></a>
+            <a title="Facebook" href="https://www.facebook.com/sanchay.javeria"><i className="fab fa-facebook"></i></a>
           </p>
-          <p>Built with <a class="anchor" href="https://github.com/drcmda/react-spring">react spring ♡</a></p>
+          <p>Built with <a className="anchor" href="https://github.com/drcmda/react-spring">react spring ♡</a></p>
         </div>
       </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route component= {Home} />
+      </Router>
     )
   }
 }
