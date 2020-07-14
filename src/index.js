78e28cf6b731f85c2c7ecf43fda79a0824a00bbe
Sanchay Javeria <sjaveria@pinterest.com>
2020-07-14 04:58:06 -0700

    Fix footer titles

--- src/index.js
+++ src/index.js
@@ -58,8 +58,8 @@ class Home extends React.Component {
             <a title="Github" href="https://www.github.com/javeria2/" target="_blank"><i className="fab fa-github"></i></a>
             <a title="LinkedIn" href="https://www.linkedin.com/in/javeria2/" target="_blank"><i className="fab fa-linkedin"></i></a>
             <a title="Medium" href="https://medium.com/@sanchay.javeria" target="_blank"><i className="fab fa-medium"></i></a>
-            <a title="Facebook" href="https://twitter.com/sanchayjaveria" target="_blank"><i className="fab fa-twitter"></i></a>
-            <a title="Facebook" href="https://www.instagram.com/sanchayjaveria" target="_blank"><i className="fab fa-instagram"></i></a>
+            <a title="Twitter" href="https://twitter.com/sanchayjaveria" target="_blank"><i className="fab fa-twitter"></i></a>
+            <a title="Instagram" href="https://www.instagram.com/sanchayjaveria" target="_blank"><i className="fab fa-instagram"></i></a>
           </p>
           {/* <p>Built with <a className="anchor" href="https://github.com/drcmda/react-spring">react spring ♡</a></p> */}
         </div>
